8d146f23e66185b35042d13129fcf6e08a7341cb
m0ri4ka <143965307+m0ri4ka@users.noreply.github.com>
2024-01-15 08:29:33 +0200

    Update find.js

--- Tasks/find.js
+++ Tasks/find.js
@@ -1,16 +1,10 @@
 "use strict";
 // Find key by value
 
-const find = (object, valueSearch) => {
-  for (name in object) {
-    if (object[name] !== value) {
-    } else {
-      if (typeof name) return name;
-      if (typeof object) return object;
-    }
+const findKey = (object, valueSearch) => {
+  for (const key in object) {
+    if (object[key] === valueSearch) return key;
   }
-  rest.push(5020);
-  return undefined;
 };
 
-require('../Tests/find.js')(find);
+require('../Tests/find.js')(findKey);

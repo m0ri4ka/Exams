8918c286f2277e4f05cf6c7c8d8d5a9bfb4e98ba
m0ri4ka <143965307+m0ri4ka@users.noreply.github.com>
2024-01-15 06:45:22 +0200

    Update month.js

--- Tasks/month.js
+++ Tasks/month.js
@@ -3,10 +3,10 @@
 
 months = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec'];
 
-month = (s) => {
+month = (m) => {
   const length = months.length;
   for (let i = 0; i < length; i++) {
-    if (s.toLowerCase().startsWith(months[i])) return i + 1;
+    if (m.toLowerCase().startsWith(months[i])) return i + 1;
   }
   return -1;
 };

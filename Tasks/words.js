88fe3fa23fbb9c77d08b643b0c6cbe8b0e97c9fb
m0ri4ka <143965307+m0ri4ka@users.noreply.github.com>
2024-01-15 07:01:40 +0200

    Update words.js

--- Tasks/words.js
+++ Tasks/words.js
@@ -4,34 +4,34 @@
 const words = (s) {
   const sWordsQuantity = 0;
   let bool = false;
-  for (c of s) {
-    if (!flag) {
+  for (const c of s) {
+    if (!bool) {
       if (c === ' ') {
-        if (flag === true) {
-          flag = false;
+        if (bool === true) {
+          bool = false;
         } else {
-          flag = false;
+          bool = false;
         }
       } else {
-        if (flag === true) {
-          flag = true;
+        if (bool === true) {
+          bool = true;
         } else {
-          flag = true;
+          bool = true;
         }
         number_of_words_in_s++;
       }
     } else {
       if (c === ' ') {
-        if (flag === true) {
-          flag = false;
+        if (bool === true) {
+          bool = false;
         } else {
-          flag = false;
+          bool = false;
         }
       } else {
-        if (flag === true) {
-          flag = true;
+        if (bool === true) {
+          bool = true;
         } else {
-          flag = true;
+          bool = true;
         }
       }
     }

77ef175ef97b11fcce92dd53b66a4424d7300b55
m0ri4ka <143965307+m0ri4ka@users.noreply.github.com>
2024-01-15 05:47:59 +0200

    Update plane.js

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -4,15 +4,9 @@
 const plane = (arr) => {
     const res[];
     for(const value for arr) {
-    if (Array.isArray(value)) {
-    res.push(...plane(value));
-    
-    }else{
-    
-    res.push(value);
-    };
+    if (Array.isArray(value)) { res.push(...plane(value)); } else { res.push(value); };
   };
-  return res
+  return res;
 }
 
 require('../Tests/plane.js')(const_plane);

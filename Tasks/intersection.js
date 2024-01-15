9f1806d87b73ea72a019af73dd5d5695483ca015
m0ri4ka <143965307+m0ri4ka@users.noreply.github.com>
2024-01-15 04:51:30 +0200

    Update intersection.js

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -2,9 +2,9 @@
 // Find an intersection of two dictionaries
 
 const intersection = (object1, object2) => {
-  const obj1keys = Object.keys(object1);
-  for (attribute_name of first_keys) {
-    if (object_1[attribute_name] === object_2[attribute_name]) {
+  const obj1Keys = Object.keys(object1);
+  for (const key of obj1Keys) {
+    if (object1[key] === object_2[key]) {
       object_2[attribute_name] = object_1[attribute_name];
     } else {
       delete object_1[attribute_name];

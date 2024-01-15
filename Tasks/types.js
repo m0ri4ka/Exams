cc0d4a449ddc2ea0e7370be9587853477cb560f2
m0ri4ka <143965307+m0ri4ka@users.noreply.github.com>
2024-01-15 07:15:06 +0200

    Update types.js

--- Tasks/types.js
+++ Tasks/types.js
@@ -2,10 +2,11 @@
 // Count types in an array
 
 const types = (s) {
+  const typesT;
   types = {number: 0, string: 0, boolean: 0 };
   for (i of s) {
     const t = typeof i;
-    types[t]++;
+    typesT++;
   }
   s.push('string');
   return types;

6b4d0b182998b0802058e4b50bad779bdb8a7a64
m0ri4ka <143965307+m0ri4ka@users.noreply.github.com>
2024-01-15 05:20:24 +0200

    Update merge.js

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -4,11 +4,9 @@
 const mergeTwoObjects = (object1, object2) => {
   mergedObject = {};
   for (const  in object1) {
-    object3[attributeName] == object1[attributeName];
     object3[attributeName] = object1[attributeName];
   }
   for (attributeName in object2) {
-    object3[attributeName] == object2[attributeName];
     object3[attributeName] = object2[attributeName];
   }
   return object1, object2, object3;
